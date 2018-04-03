d81fd439691a5288be977ca7b28c903c34ce6ca2
Andres Mauricio Rondon <amrondonp@gmail.com>
2018-04-02 20:41:37 -0500

    Add basic parser

--- Taller2/src/parser/Parser.java
+++ Taller2/src/parser/Parser.java
@@ -1,9 +1,30 @@
 package parser;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import gen.TLv2Lexer;
 import gen.TLv2Parser;
 
 public class Parser {
+	
+	private static String test = "test_cases/1/in01.txt";
+	
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		try {
+	        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(test));
+	        TLv2Lexer lexer = new TLv2Lexer(input);
+	        CommonTokenStream tokens = new CommonTokenStream(lexer);
+	        TLv2Parser parser = new TLv2Parser(tokens);
+	        ParseTree tree = parser.tl();
+	        System.out.println("El analisis sintactico ha finalizado correctamente.");
+        } catch (Exception e) {
+			e.printStackTrace();
+		}	
 	}
 }
